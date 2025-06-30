e0c1237e8cb229a26a5151d4ffaeaa0b6ad12846
Thomas Mills <mills.thoma@gmail.com>
2025-06-30 18:37:40 +0200

    initial date and time inputs

--- src/pages/ReactHookForm/BookingForm/BookingForm.tsx
+++ src/pages/ReactHookForm/BookingForm/BookingForm.tsx
@@ -1,8 +1,10 @@
-import { Grid, Heading } from '@amsterdam/design-system-react';
+import { Button, Grid, Heading, Row } from '@amsterdam/design-system-react';
 import BookingFormProvider from './BookingFormProvider';
 import TextInputControl from './components/TextInputControl/TextInputControl';
 import CheckboxControl from './components/CheckboxControl/CheckboxControl';
 import TextAreaControl from './components/TextAreaControl/TextAreaControl';
+import DateControl from './components/DateControl/DateControl';
+import TimeControl from './components/TimeControl/TimeControl';
 
 const BookingForm = () => {
   return (
@@ -32,6 +34,37 @@ const BookingForm = () => {
             registerOptions={{ required: true }}
           />
 
+          <Row>
+            <DateControl<{ startDate: string }>
+              label="Start date"
+              name="startDate"
+              testId="booking-create-start-date"
+              registerOptions={{ required: true }}
+            />
+
+            <TimeControl<{ startTime: string }>
+              label="Start time"
+              name="startTime"
+              testId="booking-create-start-time"
+              registerOptions={{ required: true }}
+            />
+          </Row>
+          <Row>
+            <DateControl<{ endDate: string }>
+              label="End date"
+              name="endDate"
+              testId="booking-create-end-date"
+              registerOptions={{ required: true }}
+            />
+
+            <TimeControl<{ endTime: string }>
+              label="End time"
+              name="endTime"
+              testId="booking-create-end-time"
+              registerOptions={{ required: true }}
+            />
+          </Row>
+
           <CheckboxControl<{ remote: boolean }>
             label="Is the meeting remote?"
             name="remote"
@@ -42,7 +75,10 @@ const BookingForm = () => {
             label="Additional comments"
             name="comments"
             testId="booking-create-comments"
+            className="ams-mb-m"
           />
+
+          <Button variant="primary">Submit</Button>
         </Grid.Cell>
       </Grid>
     </BookingFormProvider>
