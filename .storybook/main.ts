996eca15bace94663455b9c2631117b8227e6a11
Thomas Mills <mills.thoma@gmail.com>
2025-08-14 18:30:42 +0200

    storybook 9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,30 +8,25 @@ const config: StorybookConfig = {
     '../src/stories/pages/**/*.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            remarkPlugins: [remarkGfm],
-          },
+
+  addons: [{
+    name: '@storybook/addon-docs',
+    options: {
+      mdxPluginOptions: {
+        mdxCompileOptions: {
+          remarkPlugins: [remarkGfm],
         },
       },
     },
-    '@storybook/addon-themes',
-  ],
+  }, '@storybook/addon-themes'],
+
   framework: {
     name: '@storybook/react-vite',
     options: {
       strictMode: true,
     },
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
   typescript: {
     check: false,
     reactDocgen: 'react-docgen-typescript',
@@ -40,10 +35,11 @@ const config: StorybookConfig = {
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
+
   viteFinal: async (config) => {
     // Merge custom vite config if needed
     return config;
-  },
+  }
 };
 
 export default config;
